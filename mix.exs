1bc1a6e620811224bfeca9498723ea5b13393c5e
Daniil Fedotov <dfedotov@pivotal.io>
2017-01-23 18:01:36 +0000

    Do not start elixir logger

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Jwt.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :jose]]
+    [applications: [:jose]]
   end
 
   # Dependencies can be Hex packages:
